3fa5b282fe4a5b3fff54378f9d7b993b4f7db2c4
Erik Cruz <74468810+kelok3rik@users.noreply.github.com>
2022-11-20 01:04:25 -0400

    Union del proyecto

---
+++ Sistema_Medico/Procesos/registrarCobertura.php
@@ -0,0 +1,33 @@
+<?php
+
+include("../DATABASE/db.php");
+
+
+if (isset($_POST['btn_enviar'])) {
+
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $cedula = $_POST['cedula'];
+    $email = $_POST['email'];
+    $telefono = $_POST['num_telefono'];
+    $celular = $_POST['celular'];
+    $ars = $_POST['ars'];
+    $num_ars = $_POST['numars'];
+    $direccion = $_POST['direccion'];
+    if (isset($_POST["status"])) {
+        $status = 0;
+    } else {
+        $status = 1;
+    }
+
+    $consulta = "INSERT INTO cobertura(nombre,apellido,cedula,email,telefono,celular,ars,num_ars,direccion,status) VALUES ('$nombre','$apellido','$cedula','$email','$telefono','$celular','$ars','$num_ars','$direccion','$status')";
+
+    $resultado = mysqli_query($conn, $consulta);
+    if (!$resultado) {
+        die("Querry Failed");
+    }
+    
+
+   header("Location: ../Pantallas/procesoCobertura.php");
+}
+?>
